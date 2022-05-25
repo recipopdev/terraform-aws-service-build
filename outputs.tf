3913fa755308f8784c3d05ab0f79782f35de69a9
Calxus <calxus@protonmail.ch>
2022-05-25 17:21:20 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "image" {
 
 output "image_digest" {
   description = "The hash of the image that has been built"
-  value       = docker_registry_image.main.latest
+  value       = split("@", docker_registry_image.main.repo_digest)[1]
 }
